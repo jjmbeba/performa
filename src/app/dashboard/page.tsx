c914bc94249c79df6de90cfad6806ddf1d6e7910
John Johnson Mbeba <jjmbeba@gmail.com>
2024-01-29 01:04:49 +0300

    Updated redirect logic

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,26 @@
-"use server";
+"use client";
 
 import { redirect } from "next/navigation";
-import { getCurrentUser } from "../auth-actions/server/actions";
+import { getCurrentUser } from "../auth-actions/client/actions";
+import { useEffect } from "react";
+import { useAuthStore } from "@/store/authStore";
 
-const Page = async () => {
-  const user = await getCurrentUser();
+const Page = () => {
+  const [user] = useAuthStore((state) => [state.user]);
 
-  if (!user) {
-    redirect("/login");
-  }
+  useEffect(() => {
+    if (!user) {
+      redirect("/login");
+    }
+  }, []);
 
-  return <div className="px-8 pt-8 float-right">
-    <h1 className="text-2xl font-bold">Welcome, {user?.user_metadata.name}</h1>
-  </div>;
+  return (
+    <div className="px-8 pt-8 float-right">
+      <h1 className="text-2xl font-bold">
+        Welcome, {user?.user_metadata.name}
+      </h1>
+    </div>
+  );
 };
 
 export default Page;
