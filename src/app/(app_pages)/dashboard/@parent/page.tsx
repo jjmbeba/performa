ac21c34528adb4f6642e0114c42c57886eed2c36
John Johnson Mbeba <jjmbeba@gmail.com>
2024-02-01 01:35:31 +0300

    Update dashboard

--- src/app/(app_pages)/dashboard/@parent/page.tsx
+++ src/app/(app_pages)/dashboard/@parent/page.tsx
@@ -1,23 +1,41 @@
+"use client";
+
 import { getCurrentUser } from "@/app/auth-actions/server/actions";
 import React from "react";
-import ParentSummary from '@/components/ParentSummary'
+import ParentSummary from "@/components/ParentSummary";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { redirect } from "next/navigation";
 
-const page = async () => {
-  const user = await getCurrentUser();
+const page = () => {
+  const { data: user, error } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const user = await getCurrentUser();
 
-  if (user) {
-    return (
-      <div>
-        <div className="flex items-center justify-between font-bold">
-          <h1 className="text-3xl">Dashboard</h1>
-          <h2 className="text-xl font-bold">
-            Welcome, {user?.user_metadata.full_name}
-          </h2>
-        </div>
-        <ParentSummary />
-      </div>
-    );
+      return user;
+    },
+  });
+
+  if (error) {
+    toast.error(error.message);
   }
+
+  if (!user) {
+    redirect("/login");
+  }
+
+  return (
+    <div>
+      <div className="flex items-center justify-between font-bold">
+        <h1 className="text-3xl">Dashboard</h1>
+        <h2 className="text-xl font-bold">
+          Welcome, {user?.user_metadata.full_name}
+        </h2>
+      </div>
+      <ParentSummary />
+    </div>
+  );
 };
 
 export default page;
