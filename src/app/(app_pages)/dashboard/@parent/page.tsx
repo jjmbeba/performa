2e2f7186b66303fd3e9d3fa423f5febcc31120b3
John Johnson Mbeba <jjmbeba@gmail.com>
2024-02-01 01:49:50 +0300

    Refactor dashboard

--- src/app/(app_pages)/dashboard/@parent/page.tsx
+++ src/app/(app_pages)/dashboard/@parent/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { getCurrentUser } from "@/app/auth-actions/server/actions";
+import { getCurrentUser } from "@/app/auth-actions/client/actions/index";
 import React from "react";
 import ParentSummary from "@/components/ParentSummary";
 import { useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { redirect } from "next/navigation";
+import { Loader2 } from "lucide-react";
 
 const Page = () => {
-  const { data: user, error } = useQuery({
+  const { data: user, error, isLoading } = useQuery({
     queryKey: ["user"],
     queryFn: async () => {
       const user = await getCurrentUser();
@@ -21,20 +22,25 @@ const Page = () => {
     toast.error(error.message);
   }
 
-  if (!user) {
-    redirect("/login");
-  }
-
   return (
-    <div>
-      <div className="flex items-center justify-between font-bold">
-        <h1 className="text-3xl">Dashboard</h1>
-        <h2 className="text-xl font-bold">
-          Welcome, {user?.user_metadata.full_name}
-        </h2>
-      </div>
-      <ParentSummary />
-    </div>
+    <>
+      {isLoading ? (
+        <div className="h-[80dvh] w-full flex flex-col items-center justify-center">
+          <Loader2 className=" h-10 w-10 animate-spin" />
+          <p>Hold on. Loading your dashboard...</p>
+        </div>
+      ) : (
+        <div>
+          <div className="flex items-center justify-between font-bold">
+            <h1 className="text-3xl">Dashboard</h1>
+            <h2 className="text-xl font-bold">
+              Welcome, {user?.user_metadata?.full_name}
+            </h2>
+          </div>
+          <ParentSummary />
+        </div>
+      )}
+    </>
   );
 };
 
